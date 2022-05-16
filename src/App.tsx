e0a0655992e854c7e7f93890815e3f854ad82769
VanishMax <maxkors2014@gmail.com>
2022-05-16 22:11:01 +0300

    feat: add log in and out buttons and account information

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
 import { useState, useEffect } from 'react'
 import { connect, keyStores, WalletConnection } from 'near-api-js'
 
+interface Account {
+  id: string,
+  balance: string,
+}
+
 function App() {
   const [wallet, setWallet] = useState<WalletConnection | null>(null);
+  const [account, setAccount] = useState<Account | null>(null);
 
   const initNear = async () => {
     if (!wallet) {
       const near = await connect({
         keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+        nodeUrl: "https://rpc.testnet.near.org",
+        walletUrl: "https://wallet.testnet.near.org",
+        helperUrl: "https://helper.testnet.near.org",
         networkId: 'testnet',
-        nodeUrl: 'https://rpc.testnet.near.org',
         headers: {},
       });
       const wallet = new WalletConnection(near, null);
       setWallet(wallet);
+
+      if (wallet.getAccountId()) {
+        setAccount({
+          id: wallet.getAccountId(),
+          balance: (await wallet.account().state()).amount,
+        })
+      }
     }
   }
 
-  const logIn = async () => {
-    const res = wallet!.requestSignIn({
-      contractId: 'contract',
-      // methodNames: [contract.addMessage.name]
-    });
-    console.log(res);
+  const logIn = async () => wallet!.requestSignIn('example-contract.testnet', 'NEAR TRY');
+  const logOut = async () => {
+    wallet!.signOut();
+    setAccount(null);
   };
 
   useEffect(() => {
@@ -35,9 +48,22 @@ function App() {
 
   return (
     <div className="p-4">
-      <button type="button" onClick={logIn}>
-        Log In
-      </button>
+      {!account ? (
+        <>
+          <h1 className="mb-2 text-2xl font-bold">Hi! Who are you?</h1>
+          <button className="btn" type="button" onClick={logIn}>
+            Log In
+          </button>
+        </>
+      ) : (
+        <>
+          <h1 className="mb-2 text-2xl font-bold">Hi, {account.id}!</h1>
+          <p className="mb-4">Your balance is: {account.balance}</p>
+          <button className="btn" type="button" onClick={logOut}>
+            Log out
+          </button>
+        </>
+      )}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/index.css'
 
+// This is needed because the signIn function of NEAR asks for the global Buffer
+import { Buffer as BufferPolyfill } from 'buffer'
+declare var Buffer: typeof BufferPolyfill;
+// @ts-ignore
+globalThis.Buffer = BufferPolyfill
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
